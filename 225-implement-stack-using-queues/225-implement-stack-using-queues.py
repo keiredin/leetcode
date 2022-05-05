3d012bd9b5476029a849251cad29b762be2dcada
keiredin <61546450+keiredin@users.noreply.github.com>
2022-05-05 15:53:01 +0300

    Time: 25 ms (95.82%), Space: 14 MB (27.76%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.py
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.py
@@ -7,15 +7,12 @@ class MyStack:
     def push(self, x: int) -> None:
         self.stack.append(x)
         
-
     def pop(self) -> int:
         return self.stack.pop()
-        
 
     def top(self) -> int:
         return self.stack[-1]
         
-
     def empty(self) -> bool:
         return len(self.stack) == 0
         
