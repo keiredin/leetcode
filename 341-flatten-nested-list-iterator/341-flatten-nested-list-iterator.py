663fb45630ae1d688818c49979aaea72f14000ac
keiredin <61546450+keiredin@users.noreply.github.com>
2022-05-08 22:03:33 +0300

    Time: 58 ms (98.58%), Space: 17.8 MB (87.28%) - LeetHub

--- 341-flatten-nested-list-iterator/341-flatten-nested-list-iterator.py
+++ 341-flatten-nested-list-iterator/341-flatten-nested-list-iterator.py
@@ -20,24 +20,28 @@
 #        Return None if this NestedInteger holds a single integer
 #        """
 class NestedIterator:
-    def __init__(self, nestedList: [NestedInteger]):
-        
-        def flatten(nested_list):
-            res = []
-            for e in nested_list:
-                if e.isInteger():
-                    res.append(e.getInteger())
-                else:
-                    res.extend(flatten(e.getList()))
-            return res
-        
-        self.flat_list = flatten(nestedList)
-        
+    def __init__(self, nestedList: List[NestedInteger]):
+        def get(currList: List[NestedInteger]) -> Generator[int, None, None]:
+            """
+            Generator function to iterate through all nested NestedInteger objects and return its integer value.
+            :param currList: The current (nested) list of NestedIntegers to iterate through.
+            :yields:         The integer value of the current NestedInteger in the iteration.
+            """
+            for nestedInteger in currList:
+                if nestedInteger.isInteger():  # nestedInteger has a single integer value
+                    yield nestedInteger.getInteger()
+                else:                          # nestedInteger is a list of NestedIntegers
+                    yield from get(nestedInteger.getList())
+        self.generator = get(nestedList)               # Initialise the generator object with the given NestedInteger list
+        self.nextInteger = next(self.generator, None)  # Obtain the next (first) NestedInteger pre-emptively
+    
     def next(self) -> int:
-        return self.flat_list.pop(0)
+        result = self.nextInteger                      # store the current NestedInteger integer value
+        self.nextInteger = next(self.generator, None)  # Obtain the next NestedInteger pre-emptively
+        return result
     
     def hasNext(self) -> bool:
-         return len(self.flat_list)>0
+        return self.nextInteger is not None
          
 
 # Your NestedIterator object will be instantiated and called as such:
