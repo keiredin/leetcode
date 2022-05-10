8c1a8cbb3477d054c975514f93e6c770e70619ff
keiredin <61546450+keiredin@users.noreply.github.com>
2022-05-10 21:18:50 +0300

    Time: 65 ms (10.72%), Space: 13.9 MB (79.39%) - LeetHub

---
+++ 216-combination-sum-iii/216-combination-sum-iii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        self.result =[]
+        self.helper([],1,k,n)
+        return self.result
+    
+    def helper(self,path,start,k,target):
+        
+        if k ==0 and target==0:
+            self.result.append(path)
+            return 
+        
+        if k ==0 or target==0:
+            return
+        
+        for i in range(start,10):
+            self.helper(path+[i],i+1,k-1,target-i)
