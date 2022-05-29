1e2f9b2f16d1eb0213ab4c44963b8d5d49899c0c
keiredin <61546450+keiredin@users.noreply.github.com>
2022-05-29 23:34:04 +0300

    Time: 506 ms (81.03%), Space: 14.4 MB (42.28%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.py
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.py
@@ -1,6 +1,28 @@
 class Solution:
     def maxProduct(self, words: List[str]) -> int:
-         return max([len(s1) * len(s2) for s1, s2 in combinations(words, 2)  if not (set(s1) & set(s2))], default=0)
+        n=len(words)
+        
+        bit_masks = [0] * n
+        lengths = [0] * n
+        
+        for i in range(n):             
+            for c in words[i]:
+                bit_masks[i]|=1<<(ord(c) - ord('a')) # set the character bit            
+            lengths[i]=len(words[i])
+                        
+        max_val = 0
+        for i in range(n):
+            for j in range(i+1, n):
+                if not (bit_masks[i] & bit_masks[j]):
+                    max_val=max(max_val, lengths[i] * lengths[j])
+        
+        return max_val 
+
+
+
+# class Solution:
+#     def maxProduct(self, words: List[str]) -> int:
+#          return max([len(s1) * len(s2) for s1, s2 in combinations(words, 2)  if not (set(s1) & set(s2))], default=0)
 
 
 
