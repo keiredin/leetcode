37e1f123d0ec651da1ec029351711ceb28e52e36
keiredin <61546450+keiredin@users.noreply.github.com>
2022-06-07 10:43:40 +0300

    Time: 1382 ms (10.60%), Space: 27.8 MB (77.67%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,24 +1,23 @@
 class Solution:
-#     def maxSubArray(self, nums: List[int]) -> int:
-#         # kadane's algorithm
-#         maxSumSoFar, maxSumEndHere = 0, 0
+    def maxSubArray(self, nums: List[int]) -> int:
+        # kadane's algorithm
+        maxSumSoFar  = nums[0]
+        maxSumEndHere = maxSumSoFar
         
-#         for i in range(len(nums)):
-#             maxSumEndHere += nums[i]
-            
-#             if maxSumEndHere < 0:
-#                 maxSumEndHere = 0
+        for i in range(1,len(nums)):
+            maxSumEndHere = max( maxSumEndHere + nums[i], nums[i])
+            maxSumSoFar = max(maxSumSoFar, maxSumEndHere)
                 
-#             if maxSumSoFar  < maxSumEndHere:
-#                 maxSumSoFar = maxSumEndHere
-                
-#         return maxSumSoFar
+        return maxSumSoFar
 
-    # dp
-    def maxSubArray(self, nums) -> int:
-        def find(index, curr_max):
-            if index >= len(nums):
-                return float('-inf')
-            curr_max = max(nums[index], curr_max + nums[index])
-            return max(curr_max,  find(index + 1, curr_max))
-        return find(0, 0)
+#     # dp
+#     def maxSubArray(self, nums) -> int:
+#         def find(index, curr_max):
+#             if index >= len(nums):
+#                 return float('-inf')
+#             curr_max = max(nums[index], curr_max + nums[index])
+#             return max(curr_max,  find(index + 1, curr_max))
+#         return find(0, 0)
+    
+    
+    
