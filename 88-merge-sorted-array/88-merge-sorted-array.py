ce7f8e257f66b2cf2b1c6efc9ed0927e6b1f8881
keiredin <61546450+keiredin@users.noreply.github.com>
2022-06-07 12:25:55 +0300

    Time: 66 ms (21.64%), Space: 14 MB (6.04%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -3,22 +3,26 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-    
-        # approach 1 - time O(n+m) - space O(m)
-        tmp_nums1 = nums1[:m]  # copy the non zero elements of nums1
-               
-        p1 = 0
-        p2 = 0
-               
-        for p in range(n + m):
-      
-            if p2 >= n or (p1 < m and tmp_nums1[p1] <= nums2[p2]):
-                nums1[p] = tmp_nums1[p1] 
-                p1 += 1
+           
+        # approach 2 - space O(1)
+        # start merging from the ends of the lists
+        p1, p2 = m-1, n-1
+        # destination d index to insert into nums1
+        for d in range(m+n-1, -1, -1):
+            # if second list is empty, nothing more to merge
+            if p2 < 0:
+                return
+            # only merge from nums1 if there are items left to merge
+            # insert at d the larger of the two values from nums1 and nums2
+            if p1 >= 0 and nums1[p1] > nums2[p2]:                
+                nums1[d] = nums1[p1]
+                p1 -= 1
             else:
-                nums1[p] = nums2[p2]
-                p2 += 1
+                nums1[d] = nums2[p2]
+                p2 -= 1
                 
+                
+        
             
                 
                 
