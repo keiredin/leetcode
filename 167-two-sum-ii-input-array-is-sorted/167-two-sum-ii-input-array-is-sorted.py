b731fbdffa9593d10eae680841cecd37f6c13152
keiredin <61546450+keiredin@users.noreply.github.com>
2022-06-09 13:04:53 +0300

    Time: 158 ms (61.70%), Space: 15 MB (11.40%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,18 +1,36 @@
+# two pointer approach
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        for i in range(len(numbers)):
-            low = i+1
-            high = len(numbers)-1
-            while (low <= high):
-                mid = (low + high) // 2
-                if numbers[mid] == target - numbers[i]:
-                    return [i + 1, mid+1]
+        left,right = 0, len(numbers)-1
+        
+        while left < right:
+            if numbers[left] + numbers[right] == target:
+                return [left+1, right+1]
+            elif numbers[left] + numbers[right] < target:
+                left += 1
+            else:
+                right -= 1
                 
-                elif numbers[mid] > target - numbers[i]:
-                    high = mid - 1
-                else:
-                    low = mid + 1
+        return [0,0]
+
+
+
+# binary search approach
+# class Solution:
+#     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+#         for i in range(len(numbers)):
+#             low = i+1
+#             high = len(numbers)-1
+#             while (low <= high):
+#                 mid = (low + high) // 2
+#                 if numbers[mid] == target - numbers[i]:
+#                     return [i + 1, mid+1]
+                
+#                 elif numbers[mid] > target - numbers[i]:
+#                     high = mid - 1
+#                 else:
+#                     low = mid + 1
                     
-        return [-1,-1]
+#         return [-1,-1]
          
         
