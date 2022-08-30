6bd7bcec7ddc0004650e57e41e3d03cc3fc34be7
keiredin <61546450+keiredin@users.noreply.github.com>
2022-08-31 00:18:20 +0300

    Time: 63 ms (31.50%), Space: 13.9 MB (29.94%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -1,11 +1,28 @@
+# class Solution:
+#     def rotate(self, matrix: List[List[int]]) -> None:
+#         n = len(matrix[0])
+#         for i in range(n // 2 + n % 2):
+#             for j in range(n // 2):
+#                 tmp = matrix[n - 1 - j][i]
+#                 matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1]
+#                 matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i]
+#                 matrix[j][n - 1 - i] = matrix[i][j]
+#                 matrix[i][j] = tmp
+        
+    
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
-        n = len(matrix[0])
-        for i in range(n // 2 + n % 2):
+        self.transpose(matrix)
+        self.reflect(matrix)
+    
+    def transpose(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+
+    def reflect(self, matrix):
+        n = len(matrix)
+        for i in range(n):
             for j in range(n // 2):
-                tmp = matrix[n - 1 - j][i]
-                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1]
-                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i]
-                matrix[j][n - 1 - i] = matrix[i][j]
-                matrix[i][j] = tmp
-        
+                matrix[i][j], matrix[i][-j - 1] = matrix[i][-j - 1], matrix[i][j]
