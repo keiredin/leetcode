440311320b1d0ca926cef5ee97394610fba48117
keiredin <61546450+keiredin@users.noreply.github.com>
2022-09-23 00:41:18 +0300

    Time: 1055 ms (5.00%), Space: 14.6 MB (11.17%) - LeetHub

--- 696-count-binary-substrings/696-count-binary-substrings.py
+++ 696-count-binary-substrings/696-count-binary-substrings.py
@@ -1,12 +1,63 @@
 class Solution(object):
-    def countBinarySubstrings(self, s):
-        ans, prev, cur = 0, 0, 1
-        for i in range(1, len(s)):
-            if s[i-1] != s[i]:
-                ans += min(prev, cur)
-                prev, cur = cur, 1
-            else:
-                cur += 1
+    def countBinarySubstrings(self, S):
+        n = len(S)
+        # To store the total count
+        # of substrings
+        ans = 0;
 
-        return ans + min(prev, cur)
+        i = 0;
+
+        # Traversing the string
+        while (i < n) :
+
+            # Count of consecutive
+            # 0's & 1's
+            cnt0 = 0; cnt1 = 0;
+
+            # Counting subarrays of
+            # type "01"
+            if (S[i] == '0') :
+
+                # Count the consecutive
+                # 0's
+                while (i < n and S[i] == '0') :
+                    cnt0 += 1;
+                    i += 1;
+
+                # If consecutive 0's
+                # ends then check for
+                # consecutive 1's
+                j = i;
+
+                # Counting consecutive 1's
+                while (j < n and S[j] == '1') :
+                    cnt1 += 1;
+                    j += 1;
+
+            # Counting subarrays of
+            # type "10"
+            else :
+
+                # Count consecutive 1's
+                while (i < n and S[i] == '1') :
+                    cnt1 += 1;
+                    i += 1;
+
+                # If consecutive 1's
+                # ends then check for
+                # consecutive 0's
+                j = i;
+
+                # Count consecutive 0's
+                while (j < n and S[j] == '0') :
+                    cnt0 += 1;
+                    j += 1;
+
+            # Update the total count
+            # of substrings with
+            # minimum of (cnt0, cnt1)
+            ans += min(cnt0, cnt1);
+
+        # Return answer
+        return ans;
                     
