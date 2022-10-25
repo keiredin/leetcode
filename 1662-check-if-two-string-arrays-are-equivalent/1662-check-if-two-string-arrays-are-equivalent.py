6d5324463bbc0c3a2a89dd5d19380c8d96ad6686
keiredin <61546450+keiredin@users.noreply.github.com>
2022-10-25 09:34:25 +0300

    Time: 85 ms (5.01%), Space: 13.9 MB (33.11%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.py
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.py
@@ -1,3 +1,39 @@
 class Solution:
     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
-        return "".join(word1) == "".join(word2)
+        charIdx1, charIdx2 = 0,0   # inner pointers for the strings
+        wordIdx1, wordIdx2 = 0, 0  # outer pointer to iterate over the array
+        
+        while wordIdx1 < len(word1) and wordIdx2 < len(word2):
+            curWord1 = word1[wordIdx1]
+            curWord2 = word2[wordIdx2]
+            
+            if curWord1[charIdx1] != curWord2[charIdx2]:
+                return False
+            
+            
+            if charIdx2 < len(curWord2)-1:
+                charIdx2 += 1
+            else:
+                charIdx2 = 0
+                wordIdx2 += 1
+                
+            if charIdx1 < len(curWord1)-1:
+                charIdx1 += 1
+            else:
+                charIdx1 = 0
+                wordIdx1 += 1
+                
+        return wordIdx1 == len(word1) and wordIdx2 == len(word2)
+    
+#     def processInternalIndex(self,charIdx, word):
+#         if charIdx < len(word)-1:
+#             charIdx += 1
+#         else:
+#             charIdx = 0
+            
+#         return charIdx
+                
+            
+                
+                
+            
