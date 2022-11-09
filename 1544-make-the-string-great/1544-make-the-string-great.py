fe565fcae87144b138325843a416f6bd86fab977
keiredin <61546450+keiredin@users.noreply.github.com>
2022-11-09 13:51:30 +0300

    Time: 81 ms (19.80%), Space: 13.8 MB (61.84%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -1,14 +1,26 @@
 class Solution:
     def makeGood(self, s: str) -> str:
-        stack = []
+#         stack = []
         
-        for char in s:
-            if stack and ord(char) < 97 and ord(stack[-1]) - ord(char) == 32:
-                stack.pop()
-            elif stack and ord(char) >= 97 and ord(char) - ord(stack[-1])  == 32:
-                stack.pop()
-            else:
-                stack.append(char)
+#         for char in s:
+#             if stack and abs(ord(stack[-1]) - ord(char)) == 32:
+#                 stack.pop()
+            
+#             else:
+#                 stack.append(char)
                 
-        return "".join(stack)
+#         return "".join(stack)
+        
+        #two pointers approach
+        s = list(s)
+        end = 0 # points to the element after the good string
+        
+        for cur in range(len(s)):
+            if end > 0 and abs(ord(s[cur]) - ord(s[end-1])) == 32:
+                end -= 1
+            else:
+                s[end] = s[cur]
+                end += 1
+                
+        return "".join(s[:end])
         
