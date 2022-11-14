d604f8c54d952cab5d967c9bf9f887bc3e9daa73
keiredin <61546450+keiredin@users.noreply.github.com>
2022-11-14 18:56:49 +0300

    Time: 123 ms (6.82%), Space: 13.9 MB (48.79%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.py
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -1,27 +1,36 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        memo = defaultdict(int)
+#         memo = [-1]*(target + 1)
         
-        
-        def dp(curSum):
-            if curSum == target:
-                return 1
-            if curSum > target:
-                return 0
+#         def dp(curSum):
+#             if curSum == target:
+#                 return 1
+#             if curSum > target:
+#                 return 0
             
-            if curSum in memo:
-                return memo[curSum]
+#             if memo[curSum] != -1:
+#                 return memo[curSum]
             
-            ans = 0
-            for j in range(len(nums)):
-                ans += dp(curSum + nums[j]) 
+#             ans = 0
+#             for j in range(len(nums)):
+#                 ans += dp(curSum + nums[j]) 
                 
-            memo[curSum] = ans
-            return memo[curSum]
+#             memo[curSum] = ans
+#             return memo[curSum]
         
-        answ = dp(0)
-        # print(memo)
-        return answ
+#         answ = dp(0)
+#         print(memo)
+#         return answ
+    
+    
+        dp = [0]*(target + 1)
+        dp[target] = 1
+        for state in range(target)[::-1]:
+            for val in nums:
+                if state + val < len(dp):
+                    dp[state] += dp[state + val]
+       
+        return dp[0]
         
                 
                 
