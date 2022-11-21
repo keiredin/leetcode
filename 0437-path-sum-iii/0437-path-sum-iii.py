b36d56169d3c5578929dd70daa1a65f7d14cecc1
keiredin <61546450+keiredin@users.noreply.github.com>
2022-11-21 11:38:29 +0300

    Time: 241 ms (46.95%), Space: 15.2 MB (95.90%) - LeetHub

---
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -0,0 +1,68 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        pathCount = 0
+        
+        def dfs(node, target, curPath):
+            nonlocal pathCount
+            
+            if not node:
+                return 0
+            
+            curPath.append(node.val)
+            pathSum = 0
+            
+            for i in range(len(curPath)-1,-1,-1):
+                pathSum += curPath[i]
+                
+                if pathSum == target:
+                    pathCount += 1
+            
+            
+            
+            left = dfs(node.left, target, curPath)
+            right = dfs(node.right, target, curPath)
+            
+            curPath.pop()
+
+                        
+        
+        if root:
+            dfs(root, targetSum,[])
+        return pathCount
+    
+    
+#         stack = [root]
+#         self.count = 0
+#         self.targetSum = targetSum
+        
+#         # iterate over all the nodes
+#         while(stack):
+#             top = stack.pop()
+#             if top:
+#                 stack.insert(0, top.left)
+#                 stack.insert(0, top.right)
+            
+#             # traverse the tree using each node as root
+#             self.traverseTree(top, 0)
+
+#         return self.count
+
+#     def traverseTree(self, curr, summ):
+#         if not curr:
+#             return
+
+#         summ += curr.val
+#         if summ == self.targetSum:
+#             self.count += 1
+
+#         self.traverseTree(curr.left, summ)
+#         self.traverseTree(curr.right, summ)
+        
+        
+        
