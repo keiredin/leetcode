d2ad65fe13642dd80c3fa1a33374e8f58c68765d
keiredin <61546450+keiredin@users.noreply.github.com>
2022-11-21 11:45:01 +0300

    Time: 928 ms (29.84%), Space: 15.3 MB (82.03%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -6,16 +6,15 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        pathCount = 0
         
         def dfs(node, target, curPath):
-            nonlocal pathCount
+            
             
             if not node:
                 return 0
             
             curPath.append(node.val)
-            pathSum = 0
+            pathCount, pathSum = 0, 0 
             
             for i in range(len(curPath)-1,-1,-1):
                 pathSum += curPath[i]
@@ -25,16 +24,53 @@ class Solution:
             
             
             
-            left = dfs(node.left, target, curPath)
-            right = dfs(node.right, target, curPath)
+            pathCount += dfs(node.left, target, curPath) # left = dfs(node.left, target, curPath) # or 
+            pathCount += dfs(node.right, target, curPath) # right = dfs(node.right, target, curPath) # or 
             
             curPath.pop()
+            
+            return pathCount # return pathCount + left + right
 
                         
         
         if root:
-            dfs(root, targetSum,[])
-        return pathCount
+            return dfs(root, targetSum,[])
+        else: return 0
+        
+        
+        
+        
+        
+        
+#         pathCount = 0
+        
+#         def dfs(node, target, curPath):
+#             nonlocal pathCount
+            
+#             if not node:
+#                 return 0
+            
+#             curPath.append(node.val)
+#             pathSum = 0
+            
+#             for i in range(len(curPath)-1,-1,-1):
+#                 pathSum += curPath[i]
+                
+#                 if pathSum == target:
+#                     pathCount += 1
+            
+            
+            
+#             left = dfs(node.left, target, curPath)
+#             right = dfs(node.right, target, curPath)
+            
+#             curPath.pop()
+
+                        
+        
+#         if root:
+#             dfs(root, targetSum,[])
+#         return pathCount
     
     
 #         stack = [root]
