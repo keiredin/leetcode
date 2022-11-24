781f534acf01bcc5bbda5c48e9a014ccc6f8ecf0
keiredin <61546450+keiredin@users.noreply.github.com>
2022-11-24 15:46:21 +0300

    Time: 76 ms (58.51%), Space: 14 MB (60.09%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,28 +1,47 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        answ = []
-        pickedIndex = defaultdict()
-        
-        # initialize all index as not picked up
-        for i in range(len(nums)):
-            pickedIndex[i] = False
-            
-        self.recurPerm(0,[],pickedIndex,nums,answ)
-        
+        answ = []     
+        self.generatePermutation(0,nums,answ)
         return answ
-    
-    
-    def recurPerm(self,i, curList, picked, nums, answ):
-        if len(curList) == len(nums):
-            answ.append(curList.copy())
+        
+    def generatePermutation(self,idx,nums,answ):
+        if idx >= len(nums):
+            answ.append(nums.copy())
+        
+        for i in range(idx, len(nums)):
+            self.swap(i,idx,nums)
+            self.generatePermutation(idx+1, nums, answ)
+            self.swap(i,idx,nums)
+        
+    def swap(self,i,j,nums):
+        nums[i],nums[j] = nums[j], nums[i]
+
+        
+        
+#        # using visited 
+#         answ = []
+#         pickedIndex = defaultdict()
+        
+#         # initialize all index as not picked up
+#         for i in range(len(nums)):
+#             pickedIndex[i] = False
             
-        for j in range(len(nums)):
-            if picked[j] == False:
-                picked[j] = True
-                curList.append(nums[j])
-                self.recurPerm(j, curList, picked, nums, answ)
-                picked[j] = False
-                curList.pop()
+#         self.recurPerm(0,[],pickedIndex,nums,answ)
+        
+#         return answ
+    
+    
+#     def recurPerm(self,i, curList, picked, nums, answ):
+#         if len(curList) == len(nums):
+#             answ.append(curList.copy())
+            
+#         for j in range(len(nums)):
+#             if picked[j] == False:
+#                 picked[j] = True
+#                 curList.append(nums[j])
+#                 self.recurPerm(j, curList, picked, nums, answ)
+#                 picked[j] = False
+#                 curList.pop()
         
 # Time = n! * n(n for deep copy of the array)
 # space = 2n - for map and curList
