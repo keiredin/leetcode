922f59440849351af96bb8dab0ff253fbc2561c9
keiredin <61546450+keiredin@users.noreply.github.com>
2022-12-25 10:49:50 +0300

    Time: 354 ms (99.05%), Space: 17 MB (92.66%) - LeetHub

---
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def maxProfitAssignment(self, difficulty, profit, worker):
+        jobs = list(zip(difficulty, profit))
+        
+        jobs.sort()
+        ans = i = best = 0
+        
+        # def findWork(worker):
+        #     idx = bisect.bisect_right(difficulty, worker)
+        #     return idx
+        
+        for skill in sorted(worker):
+            while i < len(jobs) and skill >= jobs[i][0]:
+                best = max(best, jobs[i][1])
+                i += 1
+            ans += best
+        return ans
+            
+        
+        
+        
