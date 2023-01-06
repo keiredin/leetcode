0f2986c89c34f416f1844fd7233626584396d32f
keiredin <61546450+keiredin@users.noreply.github.com>
2023-01-06 15:26:45 +0300

    Time: 117 ms (11.01%), Space: 13.9 MB (86.55%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.py
+++ 0980-unique-paths-iii/0980-unique-paths-iii.py
@@ -1,24 +1,27 @@
 class Solution:
     def uniquePathsIII(self, A):
-        self.res = 0
-        m, n, empty = len(A), len(A[0]), 1
+        res = 0
+        m, n, emptyCellCount = len(A), len(A[0]), 1
         for i in range(m):
             for j in range(n):
                 if A[i][j] == 1:
                     x, y = (i, j)
                 elif A[i][j] == 0:
-                    empty += 1
+                    emptyCellCount += 1
 
-        def dfs(x, y, empty):
+        def backtrack(x, y, emptyCellCount):
+            nonlocal res
             if not (0 <= x < m and 0 <= y < n and A[x][y] >= 0): return
             if A[x][y] == 2:
-                self.res += empty == 0
+                res += emptyCellCount == 0
                 return
             A[x][y] = -2
-            dfs(x + 1, y, empty - 1)
-            dfs(x - 1, y, empty - 1)
-            dfs(x, y + 1, empty - 1)
-            dfs(x, y - 1, empty - 1)
+            backtrack(x + 1, y, emptyCellCount - 1)
+            backtrack(x - 1, y, emptyCellCount - 1)
+            backtrack(x, y + 1, emptyCellCount - 1)
+            backtrack(x, y - 1, emptyCellCount - 1)
             A[x][y] = 0
-        dfs(x, y, empty)
-        return self.res
+            
+        backtrack(x, y, emptyCellCount)
+
+        return res
