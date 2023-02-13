b93e1132dcc187e0f88a3c61672a55473b7d52e7
Keiredin Aman <61546450+keiredin@users.noreply.github.com>
2023-02-13 23:07:06 +0300

    Update 1091-shortest-path-in-binary-matrix.py

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -5,41 +5,21 @@ class Solution:
             return -1
         
         N = len(grid)            
-        # offsets required for all 8 directions
-        offsets = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+        dir_arr = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
         
-        q = deque()
-        q.append((0,0)) # starting point
-        visited = {(0, 0)}
+        q = deque([(1, 0, 0)])
+        visited = set([1, 0, 0])
         
+        inBound = lambda row,col: 0<= row < N and 0 <= col < N and (row,col) not in visited
         
-        # finds unvisited clear cells using 8 offsets
-        def get_neighbours(x,y):
-            for x_offset, y_offset in offsets:
-                new_row = x + x_offset
-                new_col = y + y_offset
-                
-                if 0 <= new_row < N and 0 <= new_col < N and not grid[new_row][new_col] and (new_row, new_col) not in visited:
-                    yield (new_row, new_col)                                                
-            
-        
-        current_distance = 1 # start with one clear cell
-        # standard iterative BFS traversal
         while q:
-            length = len(q)
-            
-            # loop through all the cells at the same distance
-            for _ in range(length):
-                row, col = q.popleft()
-                
-                if row == N-1 and col==N-1: # reached target
-                    return current_distance
-                
-                # loop though all valid neignbours
-                for p in get_neighbours(row, col):
-                    visited.add(p)
-                    q.append(p)
-                                    
-            current_distance+=1 # update the level or distance from source
+            dist, row, col = q.popleft()
+            if (row, col) == (N-1, N-1):
+                return dist
+            for r,c in dir_arr:
+                new_r, new_c = row+r, col + c
+                if inBound(new_r,new_c) and grid[new_r][new_c] == 0:
+                    q.append((dist+1, new_r, new_c))
+                    visited.add((new_r, new_c))
         
         return -1  
