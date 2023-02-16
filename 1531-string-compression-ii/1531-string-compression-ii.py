19e1d069099c7ad0e5e82c967638909ad26fffad
Keiredin Aman <61546450+keiredin@users.noreply.github.com>
2023-02-16 23:25:51 +0300

    Time: 2029 ms (83.59%), Space: 370.4 MB (43.59%) - LeetHub

--- 1531-string-compression-ii/1531-string-compression-ii.py
+++ 1531-string-compression-ii/1531-string-compression-ii.py
@@ -1,26 +1,24 @@
 class Solution:
     def getLengthOfOptimalCompression(self, s: str, k: int) -> int:
-		# this decorator automatically use memo with key = (start, last, last_count, left)
+        edges = set([1,99,9])
+        
         @lru_cache(None)
-        def counter(start, last, last_count, left): #count the cost of compressing from the start
+        def dp(start, last, last_count, left):
             
             if left < 0:
-                return float('inf') # this is impossible
+                return float('inf')
             if start >= len(s):
                 return 0
             if s[start] == last:
-				# we have a stretch of the last_count of the same chars, what is the cost of adding one more? 
-                incr = 1 if last_count == 1 or last_count == 9 or last_count == 99 else 0
-				# no need to delete here, if we have a stretch of chars like 'aaaaa' - we delete it from the beginning in the else delete section
-                return incr + counter(start+1, last, last_count+1, left) # we keep this char for compression
+                incr = 1 if last_count in edges else 0
+                return incr + dp(start+1, last, last_count+1, left)
             else:
                 
-				# keep this char for compression - it will increase the result length by 1 plus the cost of compressing the rest of the string 
-                keep_counter = 1 + counter(start+1, s[start], 1, left)
+                use = 1 + dp(start+1, s[start], 1, left)
                 
-				# delete this char
-                del_counter =  counter(start + 1, last, last_count, left - 1)
+				# delete counter
+                delete =  dp(start + 1, last, last_count, left - 1)
                 
-                return min(keep_counter, del_counter)
+                return min(use, delete)
             
-        return counter(0, "", 0, k)
+        return dp(0, "", 0, k)
