00c01aa841fd1335479ea63156247bf7b483262e
Keiredin Aman <61546450+keiredin@users.noreply.github.com>
2023-02-22 22:49:17 +0300

    Time: 122 ms (38.35%), Space: 72 MB (5.23%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.py
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -3,24 +3,24 @@ class Solution:
         def update(op, v):
             if op == "+": stack.append(v)
             if op == "-": stack.append(-v)
-            if op == "*": stack.append(stack.pop() * v)           #for BC II and BC III
-            if op == "/": stack.append(int(stack.pop() / v))      #for BC II and BC III
+            if op == "*": stack.append(stack.pop() * v)          
+            if op == "/": stack.append(int(stack.pop() / v))     
     
-        it, num, stack, sign = 0, 0, [], "+"
+        i, num, stack, sign = 0, 0, [], "+"
         
-        while it < len(s):
-            if s[it].isdigit():
-                num = num * 10 + int(s[it])
-            elif s[it] in "+-*/":
+        while i < len(s):
+            if s[i].isdigit():
+                num = num * 10 + int(s[i])
+            elif s[i] in "+-*/":
                 update(sign, num)
-                num, sign = 0, s[it]
-            elif s[it] == "(":                                        # For BC I and BC III
-                num, j = self.calculate(s[it + 1:])
-                it = it + j
-            elif s[it] == ")":                                        # For BC I and BC III
+                num, sign = 0, s[i]
+            elif s[i] == "(":                                        
+                num, j = self.calculate(s[i + 1:])
+                i = i + j
+            elif s[i] == ")":                                        
                 update(sign, num)
-                return sum(stack), it + 1
-            it += 1
+                return sum(stack), i + 1
+            i += 1
         update(sign, num)
         
         return sum(stack)
