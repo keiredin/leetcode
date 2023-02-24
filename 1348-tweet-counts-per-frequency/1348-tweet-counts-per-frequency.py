da7e0f8407fa7735356af74bd0ed27c6e7745ac0
Keiredin Aman <61546450+keiredin@users.noreply.github.com>
2023-02-24 22:56:52 +0300

    Time: 355 ms (84.82%), Space: 20.1 MB (25.00%) - LeetHub

---
+++ 1348-tweet-counts-per-frequency/1348-tweet-counts-per-frequency.py
@@ -0,0 +1,28 @@
+class TweetCounts:
+
+    def __init__(self):
+        self.tweets = defaultdict(list)
+        self.time = { 'minute': 60, 'hour':60*60, 'day':60*60*24}
+
+    def recordTweet(self, tweetName: str, time: int) -> None:
+        self.tweets[tweetName].append(time)
+
+    def getTweetCountsPerFrequency(self, freq: str, tweetName: str, startTime: int, endTime: int) -> List[int]:
+        
+        if freq == "minute": seconds = 60 
+        elif freq == "hour": seconds = 3600
+        else: seconds = 86400
+            
+        n = ((endTime-startTime) // seconds +1)
+        res = [0] * n
+        for num in self.tweets[tweetName]:
+            if startTime <= num <= endTime:
+                res[(num-startTime) // seconds ] += 1
+             
+        return res
+
+
+# Your TweetCounts object will be instantiated and called as such:
+# obj = TweetCounts()
+# obj.recordTweet(tweetName,time)
+# param_2 = obj.getTweetCountsPerFrequency(freq,tweetName,startTime,endTime)
