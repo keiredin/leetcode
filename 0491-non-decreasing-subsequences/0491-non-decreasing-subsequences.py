45fb870b24ec3500303aa1ed5e3d6c898779de84
Keiredin Aman <61546450+keiredin@users.noreply.github.com>
2023-03-29 22:35:19 +0300

    Time: 236 ms (60.04%), Space: 22.7 MB (29.53%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
@@ -22,3 +22,13 @@ class Solution:
             
         backtrack(0)
         return result
+    
+    
+    # def lengthOfLIS(nums):
+    #     n = len(nums)
+    #     dp = [1] * n
+    #     for i in range(1, n):
+    #         for j in range(i):
+    #             if nums[i] >= nums[j]:
+    #                 dp[i] = max(dp[i], dp[j]+1)
+    #     return max(dp)
